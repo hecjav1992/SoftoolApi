35dcbb9fd409492f2079ed5b3859d080cb102be9
hecjav1992 <easydata10@gmail.com>
2026-08-06 20:44:43 -0500

    first comm

--- Controllers/DiagnosticosController.cs
+++ Controllers/DiagnosticosController.cs
@@ -66,7 +66,7 @@ public class DiagnosticosController(AppDbContext db, PdfService pdf) : Controlle
       Marca = ingreso.Marca.Trim(),
       Modelo = ingreso.Modelo.Trim(),
       ImeiSerie = ingreso.ImeiSerie.Trim(),
-      TipoEquipo ="fg",
+      TipoEquipo = dto.TipoEquipo.Trim(),
       DiagnosticoTecnico = dto.DiagnosticoTecnico.Trim(),
       Recomendacion = dto.Recomendacion.Trim(),
       Evidencia = bytes,
--- DTOs/CrearIngresoDto.cs
+++ DTOs/CrearIngresoDto.cs
@@ -1,1 +1,15 @@
-namespace EasyData.Api.DTOs; public class CrearIngresoDto{public string NumeroIngreso{get;set;}="";public DateOnly FechaIngreso{get;set;}public string Cliente{get;set;}="";public string Telefono{get;set;}="";public string? Correo{get;set;}public string TipoEquipo{get;set;}="";public string Marca{get;set;}="";public string Modelo{get;set;}="";public string ImeiSerie{get;set;}="";public string Accesorios{get;set;}="";public string EstadoFisico{get;set;}="";public string FallaReportada{get;set;}="";public string? Observaciones{get;set;}}
+namespace EasyData.Api.DTOs; 
+public class CrearIngresoDto{
+    public string NumeroIngreso{get;set;}="";
+    public DateOnly FechaIngreso{get;set;}
+    public string Cliente{get;set;}="";
+    public string Telefono{get;set;}="";
+    public string? Correo{get;set;}
+    public string TipoEquipo{get;set;}="";
+    public string Marca{get;set;}="";
+    public string Modelo{get;set;}="";
+    public string ImeiSerie{get;set;}="";
+    public string Accesorios{get;set;}="";
+    public string EstadoFisico{get;set;}="";
+    public string FallaReportada{get;set;}="";
+    public string? Observaciones{get;set;}}
