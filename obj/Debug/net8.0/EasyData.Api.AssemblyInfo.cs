93fb8b2f8126a9c2bb17d21eb3669312237f4a72
hecjav1992 <easydata10@gmail.com>
2026-08-14 23:59:42 -0500

    act pdfIngresoService

--- obj/Debug/net8.0/EasyData.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/EasyData.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyData.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeb27002bc92b77dc091eb71be767a8d59752a46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f84f85c9cd045e0e43039723196cb36c69e14c03")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyData.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyData.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
